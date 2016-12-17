59e0d3d56cc8ae313c8acfb8cb2cd8370ce9b552
KimSJ <7577ksj@gmail.com>
2016-12-17 12:30:08 +0900

    -작업

---
+++ FireBaseStudy/UserAddViewController.swift
@@ -0,0 +1,40 @@
+//
+//  UserAddViewController.swift
+//  FireBaseStudy
+//
+//  Created by KimSeongJoon on 2016. 12. 17..
+//  Copyright © 2016년 KimSeongJoon. All rights reserved.
+//
+
+import UIKit
+import Firebase
+
+class UserAddViewController: UIViewController {
+
+    var postRef: FIRDatabaseReference!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+//        let tmpEmail = "FireBaseUser2@mail.com"
+//        
+//        postRef = FIRDatabase.database().reference()
+//        
+//        FIRAuth.auth()?.createUser(withEmail: tmpEmail, password: "112233", completion: { (user, error) in
+//            
+//            if let error = error{
+//                
+//                print("\(error.localizedDescription)")
+//            }else{
+//                print("유저(이메일) 등록")
+//                
+//                let key = self.postRef.child("posts").childByAutoId().key
+//                let post = ["uid": tmpEmail]
+//                let childUpdates = ["/posts/\(key)": post]
+//                self.postRef.updateChildValues(childUpdates)
+//            }
+//        })
+        
+    }
+
+}
---
+++ FireBaseStudy/UserListViewController.swift
@@ -0,0 +1,27 @@
+//
+//  UserListViewController.swift
+//  FireBaseStudy
+//
+//  Created by KimSeongJoon on 2016. 12. 17..
+//  Copyright © 2016년 KimSeongJoon. All rights reserved.
+//
+
+import UIKit
+import Firebase
+
+class UserListViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+
+}
